4e10e513420e66719fd6a50e714617803e26e8c9
Krishna <kkabap@gmail.com>
2017-09-07 07:52:36 +0530

    AT-002: Modified the code

--- src/Registration.java
+++ src/Registration.java
@@ -2,7 +2,7 @@
 public class Registration {
 
 	public static void main(String[] args) {
-		System.out.println("Registration functionality completed");
+		System.out.println("Successfully registation functinality finished");
 
 	}
 
